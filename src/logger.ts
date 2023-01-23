be8924b5c827ebc6fdff5353c38077ae69dac579
xtexChooser <xtexchooser@duck.com>
2023-01-23 13:39:46 +0800

    load

---
+++ src/logger.ts
@@ -0,0 +1,23 @@
+import pino from 'pino'
+
+const logger = pino({
+    transport: {
+        targets: [
+            {
+                target: 'pino-pretty',
+                options: {
+                    colorize: true
+                },
+                level: 'info'
+            }
+        ]
+    },
+    redact: {
+        paths: [
+            "req['headers']",
+            "res['headers']"
+        ]
+    }
+})
+
+export default logger
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-console.log("helsadfloworld");
+import Koa from 'koa'
+import Router from '@koa/router'
+import serve from 'koa-static'
+import pino from 'koa-pino-logger'
+import logger from './logger.js'
+
+const app = new Koa()
+
+const router = new Router()
+/*
+router.get('/', (ctx) => {
+  // ctx.router available
+  ctx.body = "test"
+  ctx.log.info('test')
+})*/
+
+app
+  .use(pino({ logger }))
+  .use(router.routes())
+  .use(router.allowedMethods())
+  .use(serve('public'))
+  .listen(3000)

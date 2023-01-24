356401e1e1df210d2108a71c39cc331f454ead0d
xtexChooser <xtexchooser@duck.com>
2023-01-24 12:29:22 +0800

    feat: disable pino-pretty

--- src/logger.ts
+++ src/logger.ts
@@ -1,17 +1,6 @@
 import pino from "pino";
 
 const logger = pino({
-  transport: {
-    targets: [
-      {
-        target: "pino-pretty",
-        options: {
-          colorize: true,
-        },
-        level: "info",
-      }
-    ],
-  },
   redact: {
     paths: ["req['headers']", "res['headers']"],
   },
